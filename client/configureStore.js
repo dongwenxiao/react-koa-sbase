ba8920a74a6d828afd4178f30e65f9b0968e9c72
dongwenxiao <cs_victor@126.com>
2017-03-30 18:04:04 +0800

    clean redux

--- client/configureStore.js
+++ client/configureStore.js
@@ -1,42 +1,28 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-// import thunk from 'redux-thunk'
-// import rootReducer from './rootReducer'
-// import { browserHistory } from 'react-router'
-// import { routerMiddleware } from 'react-router-redux'
 
 
-// const middlewares = [
-//     thunk,
-//     routerMiddleware(browserHistory)
-// ]
-
+// redux调试
 let devToolsExtension = (f) => f
-
-if (__CLIENT__) {
+if (__CLIENT__ && __DEV__) {
     if (window.devToolsExtension) {
         devToolsExtension = window.devToolsExtension()
     }
 }
 
 
-// export default function configStore(initialState) {
-
-//     const store = createStore(rootReducer, initialState, compose(
-//         applyMiddleware(...middlewares),
-//         devToolsExtension
-//     ))
-
-//     return store
-// }
-
-export function factoryConfigureStore (reducers, middlewares = []) {
-
+export function factoryConfigureStore(reducers, middlewares = []) {
     return (initialState) => {
-        const store = createStore(reducers, initialState, compose(
-            applyMiddleware(...middlewares),
-            devToolsExtension
-        ))
-
+        let store
+        if (__DEV__) {
+            store = createStore(reducers, initialState, compose(
+                applyMiddleware(...middlewares),
+                devToolsExtension
+            ))
+        } else {
+            store = createStore(reducers, initialState, compose(
+                applyMiddleware(...middlewares)
+            ))
+        }
         return store
     }
 }
